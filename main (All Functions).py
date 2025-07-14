153d0ed07fdec892a1378e21228ddf52369f65b6
tristenrodrigo <tristenrodrigo@gmail.com>
2025-07-15 11:56:37 +1200

    renaming database file and modifying main script

--- main (All Functions).py
+++ main (All Functions).py
@@ -8,7 +8,7 @@ import os
 current_user_email = None
 
 # Create/connect to the database
-conn = sqlite3.connect('listings.db')
+conn = sqlite3.connect('.db')
 cursor = conn.cursor()
 
 # Create the listings table if it doesn't exist
@@ -291,84 +291,6 @@ def sign_up_page():
         sign_up_button.place(x=10, y=160, width=370, height=40)
         sign_up_button.bind("<Button-1>", lambda event: save_user())
         
-    def undefined_second_signup_window():
-        # Creating the second window
-        second_window = Toplevel(sign_up_window)
-        second_window.title("Sign Up")
-        second_window.geometry("400x700")
-        second_window.config(bg="white")
-
-        # Creating Header for the second window
-        header = Label(second_window, text="Sign Up: Page 2", font=("Lora", 24))
-        header.config(bg="#4F6F52", fg="white")
-        header.place(x=10, y=10, width=380, height=50)
-
-        #Clothing Size Label Option 1
-        size1_label = Label(second_window, text="Clothing Size:", font=("Lora", 12))
-        size1_label.config(bg="white", fg="black")
-        size1_label.place(x=10, y=70, width=180, height=30)
-        size1_options = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"]
-        size1_var = StringVar(second_window)
-        if size1_options:
-            size1_var.set(size1_options[0])
-        else:
-            size1_var.set("Default")
-
-        #Dropdown Menu for Clothing Size Option 1
-        size1_dropdown = OptionMenu(second_window, size1_var, *size1_options)
-        size1_dropdown.config(bg='white', fg="black")
-        size1_dropdown.place(x=200, y=70, width=80, height=30)
-
-        #Clothing Size option 2
-        size2_options = ["XS", "S", "M", "L", "XL", "XXL", "XXXL"]
-        size2_var = StringVar(second_window)
-        if size2_options:
-            size2_var.set(size2_options[0])
-        else:
-            size2_var.set("Default")
-
-        #Dropdown Menu for Clothing Size Option 2
-        size2_dropdown = OptionMenu(second_window, size2_var, *size2_options)
-        size2_dropdown.config(bg='white', fg="black")
-        size2_dropdown.place(x=290, y=70, width=90, height=30)
-
-        #Shoe Size Label
-        shoe_size_label = Label(second_window, text="Shoe Size:", font=("Lora", 12))
-        shoe_size_label.config(bg="white", fg="black")
-        shoe_size_label.place(x=10, y=110, width=180, height=30)
-        shoe_size_options = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]
-        shoe_size_var = StringVar(second_window)
-        if shoe_size_options:
-            shoe_size_var.set(shoe_size_options[0])
-        else:
-            shoe_size_var.set("Default")
-
-        #Dropdown Menu
-        shoe_size_dropdown = OptionMenu(second_window, shoe_size_var, *shoe_size_options)
-        shoe_size_dropdown.config(bg='white', fg="black")
-        shoe_size_dropdown.place(x=200, y=110, width=180, height=30)
-
-        # Save user data to database
-        def save_user():
-            cursor.execute(
-                'INSERT INTO users (first_name, last_name, email, password, gender, size1, size2, shoe_size) VALUES (?, ?, ?, ?, ?, ?, ?)',
-                (
-                    first_name_entry.get(),  # first name
-                    last_name_entry.get(),  # last name
-                    email_entry.get(),
-                    password_entry.get(),
-                    gender_var.get(),
-                    size1_var.get(),
-                    size2_var.get() if size1_var.get() == "" else None,
-                    shoe_size_var.get()
-                )
-            )
-            conn.commit()
-            messagebox.showinfo("Success", "Sign up successful!")
-            second_window.destroy()
-            sign_up_window.destroy()
-            mainpage.deiconify()
-
         sign_up_button = Label(second_window, text="Sign Up", font=("Lora", 12), bg="#809D3C", fg="White")
         sign_up_button.place(x=10, y=160, width=370, height=40)
         sign_up_button.bind("<Button-1>", lambda event: save_user())
@@ -384,8 +306,6 @@ def sign_up_page():
             male_second_signup_window()
         elif gender == "Female":
             female_second_signup_window()
-        elif gender == "Other":
-            undefined_second_signup_window()
 
     # Continue Button to the next page
     continue_button = Label(sign_up_window, text="Continue", font=("Lora", 12), bg="#809D3C", fg="white")
@@ -714,7 +634,11 @@ def manage_account_page():
     #Edit User Information Button
     edit_info_button = Label(manage_account_window, text="Edit Information", font=("Lora", 12), bg="#809D3C", fg="white")
     edit_info_button.place(x=10, y=410, width=370, height=40)
-    edit_info_button.bind("<Button-1>", lambda event: edit_user_info(), manage_account_window.withdraw())
+    def on_edit_info(event):
+        edit_user_info()
+        manage_account_window.withdraw()
+
+    edit_info_button.bind("<Button-1>", on_edit_info)
 
     def edit_user_info():
         edit_window = Toplevel(manage_account_window)
@@ -808,6 +732,21 @@ def manage_account(event):
         current_user_email = None
 manage_account_button.bind("<Button-1>", manage_account)
 
-session_start_time = time.time()
+# Check for existing session
+if os.path.exists("session.txt"):
+    with open("session.txt", "r") as f:
+        saved_email = f.read().strip()
+        cursor.execute("SELECT * FROM users WHERE email=?", (saved_email,))
+        user = cursor.fetchone()
+        if user:
+            current_user_email = saved_email
+            # Hide sign in, show manage account
+            try:
+                sign_in_button.place_forget()
+                manage_account_button.place(x=270, y=650, width=120, height=40)
+            except Exception:
+                pass
+        else:
+            current_user_email = None
 
 mainpage.mainloop()
